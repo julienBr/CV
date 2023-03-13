c2b2e83d475685e6fbf25250851ca320b668fde1
julienBr <julien.bringard@outlook.fr>
2023-03-13 16:55:32 +0100

    Update Camera Animations

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,20 +1,29 @@
+using System;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
     [SerializeField] private GameObject _player;
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
 
     private void Update()
     {
         if (_player.transform.position.x <= 77.9f && _player.transform.position.z <= -0.6f)
         {
-            transform.position = new Vector3(83.93f, 2.35f, 3.93f);
-            transform.rotation = Quaternion.Euler(22.5f,225f,0f);
+            _animator.SetTrigger("FirstToSecond");
+            /*transform.position = new Vector3(83.93f, 2.35f, 3.93f);
+            transform.rotation = Quaternion.Euler(22.5f,-135f,0f);*/
         }
         else
         {
-            transform.position = new Vector3(83.93f, 2.35f, -3.93f);
-            transform.rotation = Quaternion.Euler(22.5f,-45f,0f);
+            _animator.SetTrigger("SecondToFirst");
+            /*transform.position = new Vector3(83.93f, 2.35f, -3.93f);
+            transform.rotation = Quaternion.Euler(22.5f,-45f,0f);*/
         }
     }
 }
