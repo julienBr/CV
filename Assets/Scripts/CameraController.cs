7d33a6ce1fa89e254a4113875643e3261505a55a
julienBr <julien.bringard@outlook.fr>
2023-03-13 21:32:25 +0100

    Update Camera Animations

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
@@ -14,16 +13,7 @@ public class CameraController : MonoBehaviour
     private void Update()
     {
         if (_player.transform.position.x <= 77.9f && _player.transform.position.z <= -0.6f)
-        {
-            _animator.SetTrigger("FirstToSecond");
-            /*transform.position = new Vector3(83.93f, 2.35f, 3.93f);
-            transform.rotation = Quaternion.Euler(22.5f,-135f,0f);*/
-        }
-        else
-        {
-            _animator.SetTrigger("SecondToFirst");
-            /*transform.position = new Vector3(83.93f, 2.35f, -3.93f);
-            transform.rotation = Quaternion.Euler(22.5f,-45f,0f);*/
-        }
+             _animator.SetBool("ChangeAngle", true);
+        else _animator.SetBool("ChangeAngle", false);
     }
 }
